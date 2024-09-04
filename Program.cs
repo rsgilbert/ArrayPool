86848fe9946c4ac970b4f20c03ba84596593a5e8
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-09-04 06:10:59 +0300

    Tried out MyPool

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+class Person
+{
+    public string Name { get; set; } = "";
+
+    public override string ToString()
+    {
+        return $"My name is {Name}";
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MyPool<Person> personPool = new();
+        Person person = personPool.Rent();
+        Console.WriteLine(person);
+        person.Name = "Jessie";
+        Console.WriteLine(person);
+
+        Console.WriteLine("******** 2 ************");
+        Person person2 = personPool.Rent();
+        Console.WriteLine(person2);
+        person2.Name = "Happie";
+        Console.WriteLine(person2);
+        Console.WriteLine(person);
+
+          Console.WriteLine("******** 3 ************");
+        personPool.Return(person);
+        Person person3 = personPool.Rent();
+        Console.WriteLine(person3);
+        person3.Name = "Mike";
+        Console.WriteLine(person3);
+        Console.WriteLine(person);
+
+    }
+}
+
